850f0446cf6ac3b1d137117b9c37dcaea7154b42
Jesus <Jesus.sandoval@ncic.com>
2022-12-15 21:02:47 -0600

    concept sorta complete

--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -2,9 +2,9 @@
     <div class="todoListContainer">
         <div class="heading">
             <h2 id="title">Todo List</h2>
-            <add-item-form></add-item-form>
+            <add-item-form v-on:reloadlist="getList()"></add-item-form>
         </div>
-        <list-view></list-view>
+        <list-view :items="items" v-on:reloadlist="getList()"></list-view>
     </div>
 </template>
 
@@ -15,6 +15,26 @@ export default {
     components: {
         addItemForm,
         listView,
+    },
+    data: function () {
+        return {
+            items: [],
+        };
+    },
+    methods: {
+        getList() {
+            axios
+                .get("api/items")
+                .then((response) => {
+                    this.items = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+    },
+    created() {
+        this.getList();
     },
 };
 </script>
