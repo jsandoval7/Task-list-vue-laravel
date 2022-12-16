4d5f94d7efa36114fdd8e0e577181c863cbd7f92
Jesus <Jesus.sandoval@ncic.com>
2022-12-15 20:31:55 -0600

    adding todo listing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,21 @@
 import "./bootstrap";
 import { createApp } from "vue";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+    faPlusSquare,
+    faTrash,
+    faUserSecret,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faPlusSquare, faTrash, faUserSecret);
 
 import App from "./vue/App.vue";
 
-createApp(App).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -1,1 +1,36 @@
-<template>How To Install Vue 3 in Laravel 9 with Vite - TechvBlogs</template>
+<template>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form></add-item-form>
+        </div>
+        <list-view></list-view>
+    </div>
+</template>
+
+<script>
+import addItemForm from "./addItemForm.vue";
+import listView from "./listView.vue";
+export default {
+    components: {
+        addItemForm,
+        listView,
+    },
+};
+</script>
+
+<style scoped>
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+
+.heading {
+    background: #e6e6e6;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+</style>
